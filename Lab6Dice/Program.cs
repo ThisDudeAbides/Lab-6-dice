93690b1976291afeb84f6ebcd2032419527759af
Sarah <Ms.sarahelizabethb@gmail.com>
2018-04-30 09:04:53 -0400

    first commit

--- Lab6Dice/Program.cs
+++ Lab6Dice/Program.cs
@@ -10,6 +10,39 @@ namespace Lab6Dice
     {
         static void Main(string[] args)
         {
-        }
+            Public Static int(int n)
+            {
+                //user input
+                Console.WriteLine("number");
+                string input = Console.ReadLine();
+
+                Random rnd = new Random();
+                int sum = 0;
+                int count = 0;
+
+                
+                do
+                {
+                    sum = (rnd.Next($"{1} - {input}) + (rnd.Next(1, (input));
+                    count++;
+                } while (input !=n);
+                //loop for dice roll
+                //while(true) loop
+                //generate 2 random numbers(1-x, inclusive)
+                // store number as parameter
+                // call show dice method
+                // call showdice(param 1, param 2)
+                // do you want to play again
+
+
+
+
+
+
+
+
+
+            }
+        }   
     }
 }
